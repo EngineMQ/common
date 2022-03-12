e2cec7534037bdfa8166edfa3846680166a5364d
Balázs Csaba <csaba@balazs-magyar.hu>
2022-03-12 23:17:42 +0100

    MessageId format const

--- messageTypes.ts
+++ messageTypes.ts
@@ -42,7 +42,10 @@ export const ClientMessagePublishOptions = Type.Object({
     delayMs: Type.Optional(Type.Number()),
     expirationMs: Type.Optional(Type.Number()),
 });
-export const MESSAGE_ID_FORMAT = "^[a-z0-9]{1,32}$";
+export const MESSAGE_ID_LENGTH_MAX = 32;
+export const MESSAGE_ID_MASK = `^[a-z0-9]{1,${MESSAGE_ID_LENGTH_MAX}}$`;
+export const MESSAGE_ID_ALPHABET = "0123456789abcdefghijklmnopqrstuvwxyz";
+export const MESSAGE_ID_LENGTH_DEFAULT = 20;
 
 export type ClientMessagePublish = Static<typeof ClientMessagePublish>;
 export const ClientMessagePublish = Type.Object({
